69daf6efbf77051acad6c08bcc49cc5839fa3367
Aditya Sood <adityasood314@gmail.com>
2023-12-31 17:04:33 +0530

    Create Game Screen UI.

--- app/src/main/java/com/example/comparemania/MainActivity.kt
+++ app/src/main/java/com/example/comparemania/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.comparemania
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.comparemania.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            if(binding.etRounds.text.toString() == "" || binding.etRounds.text == null) {
+                binding.etRounds.error = "Required"
+            } else {
+                val i = Intent(this,GameActivity::class.java)
+                i.putExtra("ROUNDS", binding.etRounds.text.toString())
+                startActivity(i)
+            }
+        }
     }
 }
